ad079a555675b66d58a4088ca9b2944f0bc9098a
Wahaj-Raza <zaidi4410993@cloud.neduet.edu.pk>
2023-11-18 11:42:04 +0500

    implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,46 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter, Space_Grotesk } from "next/font/google";
-import type { Metadata } from "next";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
+// import { ThemeProvider } from "@/context/ThemeProvider";
 import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  weights: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variables: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
 });
+
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
-  weights: ["300", "400", "500", "600", "700"],
-  variables: "--font-spaceGrotesk",
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
 });
+
 export const metadata: Metadata = {
   title: "DevOverflow",
   description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in Web Development, mobile app development, algorithms, data science, data structures and more",
+    "A community-driven platform for asking and answering programming questions...",
   icons: { icon: "/assets/images/site-logo.svg" },
 };
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "primary-gradient",
-          footerActionLink: "primary-test-gradient hover:test-primary-500",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={"${inter.variable} ${spaceGrotesk.variable}"}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-test-gradient hover:test-primary-500",
+            },
+          }}
+        ></ClerkProvider>
+      </body>
+    </html>
   );
 }
