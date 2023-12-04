982ef49b848471b21d1089eaf4037afe8fac8b4a
Wahaj-Raza <zaidi4410993@cloud.neduet.edu.pk>
2023-12-04 22:34:55 +0500

    Right SideBar Done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSideBar from "@/components/shared/RightSidebar";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="background-light850_dark100 relative">
@@ -10,7 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:pb-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSideBar />
       </div>
       Toaster
     </main>
