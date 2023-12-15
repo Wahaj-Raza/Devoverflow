4730236206d4664b6a4a6d93a70f9e2a043a7393
Wahaj-Raza <zaidi4410993@cloud.neduet.edu.pk>
2023-12-15 22:26:46 +0500

    Home Page

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -4,18 +4,18 @@ import Image from "next/image";
 import RenderTag from "./RenderTag";
 const RightSidebar = () => {
   const hotQuestions = [
-    { _id: 1, title: "How do i use express as a custom  server in NEXTJS" },
-    { _id: 2, title: "How do i use express as a custom  server in NEXTJS" },
-    { _id: 3, title: "How do i use express as a custom  server in NEXTJS" },
-    { _id: 4, title: "How do i use express as a custom  server in NEXTJS" },
-    { _id: 5, title: "How do i use express as a custom  server in NEXTJS" },
+    { _id: "1", title: "How do i use express as a custom  server in NEXTJS" },
+    { _id: "2", title: "How do i use express as a custom  server in NEXTJS" },
+    { _id: "3", title: "How do i use express as a custom  server in NEXTJS" },
+    { _id: "4", title: "How do i use express as a custom  server in NEXTJS" },
+    { _id: "5", title: "How do i use express as a custom  server in NEXTJS" },
   ];
   const popularTags = [
-    { _id: 1, name: "javascript", totalQuestions: 5 },
-    { _id: 2, name: "react", totalQuestions: 10 },
-    { _id: 3, name: "next", totalQuestions: 5 },
-    { _id: 4, name: "vue", totalQuestions: 4 },
-    { _id: 5, name: "redux", totalQuestions: 1 },
+    { _id: "1", name: "javascript", totalQuestions: 5 },
+    { _id: "2", name: "react", totalQuestions: 10 },
+    { _id: "3", name: "next", totalQuestions: 5 },
+    { _id: "4", name: "vue", totalQuestions: 4 },
+    { _id: "5", name: "redux", totalQuestions: 1 },
   ];
   return (
     <section
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,52 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const timeDifference = now.getTime() - createdAt.getTime();
+
+  // Convert the time difference to seconds
+  const seconds = Math.floor(timeDifference / 1000);
+
+  // Define the time units
+  const minute = 60;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const month = 30 * day;
+  const year = 365 * day;
+
+  // Calculate the time ago
+  if (seconds < minute) {
+    return `${seconds} ${seconds === 1 ? "second" : "seconds"} ago`;
+  } else if (seconds < hour) {
+    const minutes = Math.floor(seconds / minute);
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+  } else if (seconds < day) {
+    const hours = Math.floor(seconds / hour);
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+  } else if (seconds < month) {
+    const days = Math.floor(seconds / day);
+    return `${days} ${days === 1 ? "day" : "days"} ago`;
+  } else if (seconds < year) {
+    const months = Math.floor(seconds / month);
+    return `${months} ${months === 1 ? "month" : "months"} ago`;
+  } else {
+    const years = Math.floor(seconds / year);
+    return `${years} ${years === 1 ? "year" : "years"} ago`;
+  }
+};
+export const formatLargeNumber = (number: number): string => {
+  if (number >= 1000000) {
+    const millions = (number / 1000000).toFixed(1);
+    return `${millions}M`;
+  } else if (number >= 1000) {
+    const thousands = (number / 1000).toFixed(1);
+    return `${thousands}K`;
+  } else {
+    return number.toString();
+  }
+};
