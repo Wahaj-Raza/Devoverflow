a565b9bfd6082b2c465fc958c6c3d1b3782fea28
Wahaj-Raza <zaidi4410993@cloud.neduet.edu.pk>
2023-12-26 16:18:16 +0500

    Changed some code

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: Request) {
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
@@ -51,13 +51,13 @@ export async function POST(req: Request) {
     });
   }
 
-  // Get the ID and type
-
   const eventType = evt.type;
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
+
+    console.log("here");
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
@@ -67,11 +67,15 @@ export async function POST(req: Request) {
       picture: image_url,
     });
 
+    console.log(mongoUser);
+
     return NextResponse.json({ message: "OK", user: mongoUser });
   }
+
   if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
+
     // Create a new user in your database
     const mongoUser = await updateUser({
       clerkId: id,
@@ -89,9 +93,13 @@ export async function POST(req: Request) {
 
   if (eventType === "user.deleted") {
     const { id } = evt.data;
-    const deletedUser = await deleteUser({ clerkId: id! });
+
+    const deletedUser = await deleteUser({
+      clerkId: id!,
+    });
+
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return new Response("", { status: 200 });
+  return NextResponse.json({ message: "OK" });
 }
