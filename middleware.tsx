c4a59ed7f7af7d01558ce76701caa021d60bf6c7
Wahaj-Raza <zaidi4410993@cloud.neduet.edu.pk>
2023-12-27 12:12:15 +0500

    Changed middleware.ts

--- middleware.tsx
+++ middleware.tsx
@@ -10,7 +10,7 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+  ignoredRoutes: ["/api/chatgpt"],
 });
 
 export const config = {
