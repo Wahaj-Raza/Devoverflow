c5b44db79f7634dc9a5caa46daa544365ec499a0
Wahaj-Raza <zaidi4410993@cloud.neduet.edu.pk>
2023-12-30 14:17:03 +0500

    tag details page done

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,8 +2,14 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
-import Tag from "@/database/tag.model";
+import {
+  GetAllTagsParams,
+  GetQuestionsByTagIdParams,
+  GetTopInteractedTagsParams,
+} from "./shared.types";
+import Tag, { ITag } from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
+import Question from "@/database/question.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -32,6 +38,48 @@ export async function getAllTags(params: GetAllTagsParams) {
     return { tags };
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+}
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    connectToDatabase();
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+
+    // for Pagination => caluclate the number of posts to skip based on the pageNumber and pageSize
+    // const skipAmount = (page - 1) * pageSize;
+
+    const tagFilter: FilterQuery<ITag> = { _id: tagId };
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: "questions",
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+        // skip: skipAmount,
+        limit: pageSize + 1, // +1 to check if there is next page
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!tag) {
+      throw new Error("❌🔍 Tag not found 🔍❌");
+    }
+
+    const isNext = tag.questions.length > pageSize;
+
+    const questions = tag.questions;
+
+    return { tagTitle: tag.name, questions, isNext };
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
     throw error;
   }
 }
